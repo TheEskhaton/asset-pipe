ad6d1424ce32dbe29fb2961ca1682313d204d324
theeskhaton <theeskhaton@gmail.com>
2013-08-29 11:09:21 +0200

    Implemented functions on prototype instead of this, separated code

--- app.js
+++ app.js
@@ -1,41 +1,9 @@
-var sort = require('./lib/dependency-sorter');
-var AssetPipe = function( config, basePath ) {
-     /* Events */
-    var pipeEvents = []
-    var PipeEvent = function(name){
-        this.name = name;
-        this.handlers = [];
-    };
 
-    this.on = function(evtName, fn){
-            var hasEvents = false;
-            for(var key in pipeEvents){
-                var val = pipeEvents[key];
-                 if(val.name === evtName){
-                    val.handlers.push(fn);
-                    hasEvents = true;
-                }
-            }
-            if(!hasEvents){
-                var newGlobal = new PipeEvent(evtName);
-                newGlobal.handlers.push(fn);
-                pipeEvents.push(newGlobal);
-            }
-    };
-    this.fire = function(evtName){
-        for(var key in pipeEvents){
-            var val = pipeEvents[key];
-            if(val.name === evtName){
-                for(var handlerKey in val.handlers){
-                    var handler = val.handlers[handlerKey];
-                    handler();
-                }
-            }
-        }
-    };
-    /* Init */
+var extend = require('./lib/extend');
+
+var AssetPipe = function( config, basePath ) {
     this.fire('init');
-    var config = config || require('./assets'); 
+    this.config = config = config || require('./assets'); 
     if(!config) throw new Error('Please provide a configuration');
     if(config.environment){
         if(config.environment === 'development'){
@@ -48,30 +16,12 @@ var AssetPipe = function( config, basePath ) {
         }
     }
     this.basePath = basePath || '/';
-    
-    
-    this.buildScriptTags = function(){
-        var scriptTags = '';
-        var sortedScripts = sort(config.scripts);
-        for(var key in sortedScripts){
-            var scriptName = sortedScripts[key];
-            var script = config.scripts[scriptName];
-            scriptTags += '<script src="'+this.basePath+script.path+'" type="text/javascript"></script>';  
-        }
 
+}
+extend(AssetPipe.prototype, require('./lib/event-functions'));
+extend(AssetPipe.prototype, require('./lib/builder-functions'));
 
-        return scriptTags;
-    }
-    this.buildCssTags = function(){
-        var cssTags = '';
-        for(var css in config.styles){
-            cssTags+= '<link href="'+this.basePath+config.styles[css]+'" rel="stylesheet" />';
-        }
-        return cssTags;
-    }
-
-
-    this.parse = function(html){
+AssetPipe.prototype.parse = function(html){
         this.fire('beforeParse');
         var scripts = this.buildScriptTags();
         var styles = this.buildCssTags();
@@ -79,6 +29,5 @@ var AssetPipe = function( config, basePath ) {
         this.fire('afterParse');
         return parsedHtml; 
     } 
-}
 
-module.exports = AssetPipe; 
+module.exports = AssetPipe;
